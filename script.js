63168cff3d54cbacb5b7e06aa7ce63c51fb4a7a8
branden <bsta520@gmail.com>
2017-07-20 20:31:06 +0000

    added layout for hair photos

--- script.js
+++ script.js
@@ -1,8 +1,14 @@
 /*global $*/
 $(document).ready(function(){
   
+// backgound images scroll different speed
   $('.parallax').parallax();
+  
+//when on mobile have a collapsed menu 
   $(".button-collapse").sideNav();
+  
+//images take up whole screen on click
+  $('.materialboxed').materialbox();
   
   
   
