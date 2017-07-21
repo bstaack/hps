a4dbf85c68d6ea17efe238116cbf9b23d0a31c5f
branden <bsta520@gmail.com>
2017-07-21 00:00:49 +0000

    added scrollspy for auto scrolling

--- script.js
+++ script.js
@@ -1,18 +1,20 @@
 /*global $*/
 $(document).ready(function(){
+      
+//when on mobile have a collapsed menu 
+  $(".button-collapse").sideNav();
   
 // backgound images scroll different speed
   $('.parallax').parallax();
   
-//when on mobile have a collapsed menu 
-  $(".button-collapse").sideNav();
-  
 //images take up whole screen on click
   $('.materialboxed').materialbox();
-  
+
+//contact modal   
   $('.modal').modal();
-  
-  
+
+//auto scroll depending on what section is clicked
+  $('.scrollspy').scrollSpy();
   
   
   
