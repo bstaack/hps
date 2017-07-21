000c6d9390522b148c8df1aef90e673ed924d175
branden <bsta520@gmail.com>
2017-07-21 15:13:23 +0000

    updated slider to show on smaller screens

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ $(document).ready(function(){
   
   var isMobile = window.matchMedia("only screen and (max-width: 1200px)");
   var hairHtml = "";
-  var hairGallery = [["assets/images/bstaack.jpg", "some of my best work"], ["assets/images/bstaack.jpg", "heyoh"], ["assets/images/bstaack.jpg", "iasipd"]];
+  var hairGallery = [["assets/images/bstaack.jpg", "some of my best work"], ["assets/images/bstaack.jpg", "heyoh"], ["assets/images/bstaack.jpg", "iasipd"], ["assets/images/bstaack.jpg", "some of my best work"], ["assets/images/bstaack.jpg", "heyoh"], ["assets/images/bstaack.jpg", "iasipd"]];
   
 // backgound images scroll different speed
   $('.parallax').parallax();
@@ -23,25 +23,33 @@ $(document).ready(function(){
       hairHtml += "<img class='materialboxed hair-photos' data-caption='" + hairGallery[i][1] + "' src=" + hairGallery[i][0] + ">";
     }
     $("#hair-photos-div").html(hairHtml);
-    $(".hair-photos").css({"width": "400px", "margin": ".5em"});
+    $(".hair-photos").css({"width": "400px", "margin-right": ".4em"});
       //images take up whole screen on click
     $('.materialboxed').materialbox();  
   };
 
-
+//populate slider with photos from the hairGallery array
+  function hairPhotosSlider(){
+    for (let i = 0; i < hairGallery.length; i++) {
+      hairHtml += "<li><img src=" + hairGallery[i][0] + "><div class='caption left-align'><h5 class='light grey-text text-lighten-3'>" + hairGallery[i][1] + "</h5></div></li>";
+    }
+    $("#hair-slides").html(hairHtml);
+    let x = $("#hair-slides").width();
+    $('.slider').slider(); 
+    $(".slider, #hair-slides").height(x);
+  };
   
 
   
 
   
 //if on mobile put a slider for images
-  if(isMobile.matches) {
-      $('.slider').slider();
-    } else {
-//run hair gallery if not mobile screen  
-        $(".slider").hide();
-        hairPhotosGallery(); 
-    }
+  if (isMobile.matches) { 
+    hairPhotosSlider();
+    } else { 
+      $(".slider").hide(); 
+      hairPhotosGallery();
+  }
   
   
   
