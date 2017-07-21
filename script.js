6a97ae0a673d42b340900f92ea70e35dc30c6193
branden <bsta520@gmail.com>
2017-07-21 14:39:12 +0000

    added slider on smaller screen instead of multi image gallery

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 /*global $*/
 $(document).ready(function(){
   
+  var isMobile = window.matchMedia("only screen and (max-width: 1200px)");
   var hairHtml = "";
   var hairGallery = [["assets/images/bstaack.jpg", "some of my best work"], ["assets/images/bstaack.jpg", "heyoh"], ["assets/images/bstaack.jpg", "iasipd"]];
   
@@ -17,22 +18,34 @@ $(document).ready(function(){
   $(".button-collapse").sideNav();
 
 //populate gallery with photos from the hairGallery array  
-  function hairPhotos(){
+  function hairPhotosGallery(){
     for (let i = 0; i < hairGallery.length; i++) {
       hairHtml += "<img class='materialboxed hair-photos' data-caption='" + hairGallery[i][1] + "' src=" + hairGallery[i][0] + ">";
     }
     $("#hair-photos-div").html(hairHtml);
+    $(".hair-photos").css({"width": "400px", "margin": ".5em"});
+      //images take up whole screen on click
+    $('.materialboxed').materialbox();  
   };
+
+
   
-hairPhotos();  
+
   
-//images take up whole screen on click
-  $('.materialboxed').materialbox();  
+
+  
+//if on mobile put a slider for images
+  if(isMobile.matches) {
+      $('.slider').slider();
+    } else {
+//run hair gallery if not mobile screen  
+        $(".slider").hide();
+        hairPhotosGallery(); 
+    }
   
   
   
   
-  
-  
+
   
 });
