7359fcaafceed311a932b03e8af341d4014a9bdd
branden <bsta520@gmail.com>
2017-07-21 14:09:31 +0000

    added an array of images so hair gallery can populate dynamically

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
 /*global $*/
 $(document).ready(function(){
   
+  var hairHtml = "";
+  var hairGallery = [["assets/images/bstaack.jpg", "some of my best work"], ["assets/images/bstaack.jpg", "heyoh"], ["assets/images/bstaack.jpg", "iasipd"]];
+  
 // backgound images scroll different speed
   $('.parallax').parallax();
-  
-//images take up whole screen on click
-  $('.materialboxed').materialbox();
 
 //contact modal   
   $('.modal').modal();
@@ -15,6 +15,22 @@ $(document).ready(function(){
 
 //when on mobile slide out menu  
   $(".button-collapse").sideNav();
+
+//populate gallery with photos from the hairGallery array  
+  function hairPhotos(){
+    for (let i = 0; i < hairGallery.length; i++) {
+      hairHtml += "<img class='materialboxed hair-photos' data-caption='" + hairGallery[i][1] + "' src=" + hairGallery[i][0] + ">";
+    }
+    $("#hair-photos-div").html(hairHtml);
+  };
+  
+hairPhotos();  
+  
+//images take up whole screen on click
+  $('.materialboxed').materialbox();  
+  
+  
+  
   
   
   
