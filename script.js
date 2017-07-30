da2597eb00af71182e071dd26c7de59d6d5d557b
branden <bsta520@gmail.com>
2017-07-30 22:58:26 +0000

    maps

--- script.js
+++ script.js
@@ -60,23 +60,7 @@ $(document).ready(function(){
   // }  
   
   // galleryOrSlider();
-
-  function initMap() {
-  			    var xoxo = {lat: 36.346172, lng: -82.400420};
-  			    var map = new google.maps.Map(document.getElementById('map'), {
-  			      zoom: 13,
-  			      scrollwheel: false,
-  			      mapTypeControl: true,
-  			      center: xoxo
-  			    });
-  			    var marker = new google.maps.Marker({
-  			      position: xoxo,
-  			      map: map
-  			    });
-  			  }
   			  
-  initMap();			  
-
 //when clicked changes the map to view driving directions 
   $("#nav-btn").click(function(){
     var map, infoWindow;
