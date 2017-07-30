04176e4184d85f5589f55e5f70221de861e0cf99
branden <bsta520@gmail.com>
2017-07-30 14:23:55 +0000

    moved maps funciton to script file

--- script.js
+++ script.js
@@ -1,4 +1,9 @@
 /*global $*/
+/*global navigator*/
+/*global google*/
+/*global bounds*/
+
+
 
 $(document).ready(function(){
  
@@ -57,7 +62,51 @@ $(document).ready(function(){
   galleryOrSlider();
   
     
-
+   var map, infoWindow;
+      if (navigator.geolocation) { //Checks if browser supports geolocation
+		   navigator.geolocation.getCurrentPosition(function (position) {                                                              //This gets the
+		     var latitude = position.coords.latitude;                    //users current
+		     var longitude = position.coords.longitude;                 //location
+		     var coords = new google.maps.LatLng(latitude, longitude); //Creates variable for map coordinates
+		     var directionsService = new google.maps.DirectionsService();
+		     var directionsDisplay = new google.maps.DirectionsRenderer();
+		     var mapOptions = //Sets map options
+									     {
+									       zoom: 15,  //Sets zoom level (0-21)
+									       scrollwheel: false,
+									       center: coords, //zoom in on users location
+									       mapTypeControl: true, //allows you to select map type eg. map or satellite
+									       navigationControlOptions:
+									       {
+									         style: google.maps.NavigationControlStyle.SMALL //sets map controls size eg. zoom
+									       },
+									       mapTypeId: google.maps.MapTypeId.ROADMAP //sets type of map Options:ROADMAP, SATELLITE, HYBRID, TERRIAN
+									     };
+		     map = new google.maps.Map(document.getElementById("map"), mapOptions /*Creates a new map using the passed optional parameters in the mapOptions parameter.*/);
+		     directionsDisplay.setMap(map);
+		     directionsDisplay.setPanel(document.getElementById('panel'));
+		     var request = {
+		       origin: coords,
+		       destination: '36.346172, -82.400420',
+		       travelMode: google.maps.DirectionsTravelMode.DRIVING
+		     };
+		
+		     directionsService.route(request, function (response, status) {
+		       if (status == google.maps.DirectionsStatus.OK) {
+		         directionsDisplay.setDirections(response);
+		       }
+		     });
+		   });
+			}
+			
+			 google.maps.event.addListenerOnce(map, 'bounds_changed', function(event) {
+			  this.setZoom(map.getZoom()-1);
+			
+			  if (this.getZoom() > 15) {
+			    this.setZoom(15);
+				  }
+				});
+				map.fitBounds(bounds);
 
   
   
